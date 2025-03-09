aac2fef4021dc3507892947c4536732fa52eb85f
LuisPozo-06 <luispg132@gmail.com>
2025-03-08 22:06:56 -0500

    Pruebas html

--- src/main/java/pe/clinica/demo/Controller/administrativoController.java
+++ src/main/java/pe/clinica/demo/Controller/administrativoController.java
@@ -1,4 +1,10 @@
 package pe.clinica.demo.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class administrativoController {
+    @GetMapping("/administrativo")
+    public String administrativo () {return "administrativo";}
 }
