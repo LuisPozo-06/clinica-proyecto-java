1d6b7cca9b08cebe5045aa647e71b9bae1f407a0
batuneitor <serrott16@gmail.com>
2025-04-13 08:22:26 -0500

    HTML I

--- src/main/java/pe/clinica/demo/service/PacienteService.java
+++ src/main/java/pe/clinica/demo/service/PacienteService.java
@@ -1,7 +1,6 @@
 package pe.clinica.demo.service;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pe.clinica.demo.model.PacienteModel;
 import pe.clinica.demo.repository.PacienteRepository;
